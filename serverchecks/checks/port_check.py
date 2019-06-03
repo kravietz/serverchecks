c729bd68248588c33383e0c84639ba610fe30396
Pawel Krawczyk <616047+kravietz@users.noreply.github.com>
2019-06-03 19:57:54 +0100

    Rewrite using class

--- serverchecks/checks/port_check.py
+++ serverchecks/checks/port_check.py
@@ -1,12 +1,21 @@
 import socket
 
 from serverchecks import Outcome
+from serverchecks.checks import AbstractCheck
 
 
-async def port_test(host: str, port: int) -> Outcome:
-    timeout: float = 3.0
-    try:
-        with socket.create_connection((host, port), timeout) as sock:
-            return Outcome(True, f'Connection to {host}:{port} successful')
-    except OSError as e:
-        return Outcome(False, f'Connection to {host}:{port} failed: {e}')
+class PortCheck(AbstractCheck):
+    def __init__(self, **kwargs) -> None:
+        self.host: str = kwargs.get('host')
+        self.port: int = kwargs.get('port')
+        self.timeout: float = kwargs.get('timeout', 2.0)
+
+    async def check(self) -> Outcome:
+        try:
+            with socket.create_connection((self.host, self.port), self.timeout) as sock:
+                return Outcome(True, f'TCP cnnection to {self.host}:{self.port} successful')
+        except OSError as e:
+            return Outcome(False, f'TCP connection to {self.host}:{self.port} failed: {e}')
+
+
+check_class = PortCheck
