977876093f19078d3c96d504cab2230fdd6e1c70
Pawel Krawczyk <616047+kravietz@users.noreply.github.com>
2019-06-03 19:07:28 +0100

    Reimplement using class

--- serverchecks/checks/url_check.py
+++ serverchecks/checks/url_check.py
@@ -3,17 +3,27 @@ from urllib.error import URLError
 from urllib.request import urlopen
 
 from serverchecks import Outcome
+from serverchecks.checks import AbstractCheck
 
 
-async def check(url: str, code: int = 200) -> Outcome:
-    timeout: float = 3.0
-    try:
-        ret = urlopen(url, timeout=timeout)
-    except (URLError, socket.timeout) as e:
-        return Outcome(False, f'URL {url} failed: {e}')
-    else:
-        ret_code = ret.getcode()
-        if ret_code == code:
-            return Outcome(True, f'URL {url} returned code {ret_code}')
+class UrlCheck(AbstractCheck):
+
+    def __init__(self, **kwargs) -> None:
+        self.url: str = kwargs.get('url')
+        self.expect_code: int = kwargs.get('expect_code', 200)
+
+    async def check(self) -> Outcome:
+        timeout: float = 3.0
+        try:
+            ret = urlopen(self.url, timeout=timeout)
+        except (URLError, socket.timeout) as e:
+            return Outcome(False, f'URL {self.url} failed: {e}')
         else:
-            return Outcome(False, f'URL {url} returned code {ret_code} vs expected {code}')
+            ret_code: int = ret.getcode()
+            if ret_code == self.expect_code:
+                return Outcome(True, f'URL {self.url} returned code {ret_code}')
+            else:
+                return Outcome(False, f'URL {self.url} returned code {ret_code} vs expected {self.expect_code}')
+
+
+check_class = UrlCheck
